e532ec3e06b7e7538c45f28e0680fb06a50d5be0
Mathieu Darrigade <mathieu.darrigade@clevy.io>
2021-04-21 08:51:20 +0200

    try to print output

--- actions/saveBot/index.js
+++ actions/saveBot/index.js
@@ -1,7 +1,11 @@
 const request = require('superagent');
 
 (async () => {
-  const bot = await request.get(`${process.env.CONSOLE_URL}}/bots/${process.env.BOT_ID}`)
-    .set('Accept', 'application/json');
-  return bot;
-})
+  console.log('- - - - - - - - saveBot() - - - - - - -')
+  console.log('console_url: ', process.env.CONSOLE_URL);
+  console.log('bot_id: ', process.env.BOT_ID);
+  console.log('url: ', `${process.env.CONSOLE_URL}}/bots/${process.env.BOT_ID}`)
+  const bot = await request.get(`${process.env.CONSOLE_URL}/bots/${process.env.BOT_ID}`)
+    .set('Authorization', `Bearer ${process.env.JWT}`);
+  console.log(bot);
+})();
