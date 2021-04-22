71c203201d36bd29807a7e44a0480702127e443b
Mathieu Darrigade <darrigade40@gmail.com>
2021-04-22 13:21:18 +0200

    test action github

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { BotService } = require('./bot.service');
+// const core = require('@actions/core');
+// const github = require('@actions/github');
+// const { BotService } = require('./bot.service');
 
-(async () => {
-  try {
-    // Get the JSON webhook payload for the event that triggered the workflow.
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log(payload);
-    // Retrieve the params passed through the workflow yml file.
-    if (core.getInput('save')) await BotService.saveBot();
-    if (core.getInput('build')) await BotService.buildBot();
-    if (core.getInput('create_label')) {
-      const payload = JSON.stringify(github.context.payload, undefined, 2);
-      const tag_name = payload.ref.replace('refs/tags/', '');
-      await BotService.createLabel();
-    }
-
-  }
-  catch (err) {
-    core.setFailed(err.message);
-  }
-})();
+// (async () => {
+//   try {
+//     // Get the JSON webhook payload for the event that triggered the workflow.
+//     const payload = JSON.stringify(github.context.payload, undefined, 2);
+//     // Retrieve the params passed through the workflow yml file.
+//     if (process.env.INPUT_SAVE) await BotService.saveBot();
+//     if (process.env.INPUT_BUILD) await BotService.buildBot();
+//     if (process.env.INPUT_CREATE_LABEL) {
+//       const payload = JSON.stringify(github.context.payload, undefined, 2);
+//       const label_name = payload.ref.replace('refs/tags/', '');
+//       await BotService.createLabel(label_name);
+//     }
+//     if (process.env.INPUT_DELETE_LABEL) {
+//       const payload = JSON.stringify(github.context.payload, undefined, 2);
+//       const label_name = payload.ref.replace('refs/tags/', '');
+//       await BotService.deleteLabel(label_name);
+//     }
+//   }
+//   catch (err) {
+//     core.setFailed(err.message);
+//   }
+// })();
